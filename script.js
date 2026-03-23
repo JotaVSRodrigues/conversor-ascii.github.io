4f6fe8d62974f5b59e37f917f309bcee8140b967
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-03-23 18:57:27 -0300

    resolucao de erros

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ function printLine(text) {
 // CRIA NOVAS LINHAS DE COMANDO
 function createPrompt() {
     const line = document.createElement("div")
-
+    
     const span = document.createElement("span")
     span.textContent = "sptech@joao:~$ "
-
+    
     const input = document.createElement("input")
     input.type = "text"
-
+    
     line.appendChild(span)
     line.appendChild(input)
     output.appendChild(line)
     
     input.focus() // coloca o foco do teclado no elemento input
-
+    
     // Quando apertar ENTER
     input.addEventListener("keydown", function (e) {
         if (e.key == "Enter") {
             const command = input.value
-
+            
             input.disabled = true // para deixar o input antigo travado na tela (historico)
             processCommand(command)
-
+            
             createPrompt()
         }
     })
@@ -43,14 +43,6 @@ function convert(value, fromBase, toBase) {
     return decimal.toString(toBase).toUpperCase()
 }
 
-
-// DETECTA BASE AUTOMATICAMENTE
-function detectBase(value) {
-    if (/^[01]+$/.test(value)) return 2
-    if (/^[0-7]+$/.test(value)) return 8
-    if (/^[0-9]+$/.test(value)) return 10
-    return 16
-}
 
 // TRADUZ O NOME DA BASE
 function getBase(name) {
@@ -69,13 +61,19 @@ function processCommand(cmd) {
     const parts = cmd.split(" ")
 
     if (parts[0] === "convert") {
-        const value = parts[1]
-        const toBaseName = parts[3]
 
-        let fromBase = detectBase(value)
+        if (parts.length != 5 || parts[3] != "to") {
+            printLine("Comando inválido")
+            return 
+        }
+        const fromBaseName = parts[1]
+        const value = parts[2]
+        const toBaseName = parts[4]
+
+        let fromBase = getBase(fromBaseName)
         let toBase = getBase(toBaseName)
 
-        if (toBase == null) {
+        if (toBase == null || fromBase == null) {
             printLine("Base inválida.")
             return
         }
@@ -95,7 +93,8 @@ function processCommand(cmd) {
     // comando: help
     else if (cmd == "help") {
         printLine("Comandos disponíveis:")
-        printLine("convert <valor> to <base>")
+        printLine("convert <base> <valor> to <base>")
+        printLine("Exemplo: convert hexadecimal FE359A to binary")
         printLine("Bases: binary, octal, decimal, hexadecimal")
         printLine("clear - limpa a tela")
 
@@ -113,8 +112,8 @@ function processCommand(cmd) {
 // INICIALIZACAO DO PROGRAMA
 printLine(" ")
 printLine(" ")
-printLine("Digite: convert <valor> to <base>")
-printLine("Ex: convert 10 to binary")
+printLine("Digite: convert <base> <valor> to <base>")
+printLine("Ex: convert decimal 250 to binary")
 printLine("Digite 'help' para obter ajuda.")
 printLine("")
 
